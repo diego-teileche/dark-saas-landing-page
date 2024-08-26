93bcfacf616895b85133d938f738bcc973d05965
diego-teileche <160079120+diego-teileche@users.noreply.github.com>
2024-08-26 19:44:24 -0300

    Product Showcase
    - Title added
    - Paragraph added
    - Image added

--- src/components/ProductShowcase.tsx
+++ src/components/ProductShowcase.tsx
@@ -1,5 +1,21 @@
-import appScreen from "../assets/images/app-screen.png";
+import Image from "next/image"
+import appScreen from "../assets/images/app-screen.png"
 
 export const ProductShowcase = () => {
-  return null;
-};
+  return (
+    <section className="bg-black text-white">
+      <div className="container">
+        <h2>Intuitive interface</h2>
+        <p>
+          Celebrate the joy of accomplishment with an app design to track your
+          progress, motivate your efforts, and celebrate your successes, one
+          task at a time
+        </p>
+      </div>
+      <Image
+        src={appScreen}
+        alt="The product screenshot | SaaS Landing Page built by Diego Tech"
+      />
+    </section>
+  )
+}
