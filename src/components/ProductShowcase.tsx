528a52347efc4df7e521239ee833b8aa329c0b5a
diego-teileche <160079120+diego-teileche@users.noreply.github.com>
2024-08-26 20:02:04 -0300

    Product Showcase | Styles added

--- src/components/ProductShowcase.tsx
+++ src/components/ProductShowcase.tsx
@@ -3,19 +3,26 @@ import appScreen from "../assets/images/app-screen.png"
 
 export const ProductShowcase = () => {
   return (
-    <section className="bg-black text-white">
+    <section className="bg-gradient-to-b from-black to-[#5d2cab] py-[72px] text-white sm:py-24">
       <div className="container">
-        <h2>Intuitive interface</h2>
-        <p>
-          Celebrate the joy of accomplishment with an app design to track your
-          progress, motivate your efforts, and celebrate your successes, one
-          task at a time
-        </p>
+        <h2 className="text-center text-5xl font-bold tracking-tighter sm:text-6xl">
+          Intuitive interface
+        </h2>
+        <div className="mx-auto max-w-xl">
+          <p className="mt-5 text-center text-xl text-white/70">
+            Celebrate the joy of accomplishment with an app design to track your
+            progress, motivate your efforts, and celebrate your successes, one
+            task at a time
+          </p>
+        </div>
       </div>
-      <Image
-        src={appScreen}
-        alt="The product screenshot | SaaS Landing Page built by Diego Tech"
-      />
+      <div className="mx-4 mt-12 flex justify-center">
+        <Image
+          src={appScreen}
+          className=""
+          alt="The product screenshot | SaaS Landing Page built by Diego Tech"
+        />
+      </div>
     </section>
   )
 }
