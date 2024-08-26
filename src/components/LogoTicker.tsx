66bd574d222fe067f873ef0d1449c56bf8a27e26
diego-teileche <160079120+diego-teileche@users.noreply.github.com>
2024-08-26 18:45:13 -0300

    LogoTicker | Logos and title added

--- src/components/LogoTicker.tsx
+++ src/components/LogoTicker.tsx
@@ -1,19 +1,44 @@
-import acmeLogo from "../assets/images/acme.png";
-import quantumLogo from "../assets/images/quantum.png";
-import echoLogo from "../assets/images/echo.png";
-import celestialLogo from "../assets/images/celestial.png";
-import pulseLogo from "../assets/images/pulse.png";
-import apexLogo from "../assets/images/apex.png";
+import acmeLogo from "../assets/images/acme.png"
+import quantumLogo from "../assets/images/quantum.png"
+import echoLogo from "../assets/images/echo.png"
+import celestialLogo from "../assets/images/celestial.png"
+import pulseLogo from "../assets/images/pulse.png"
+import apexLogo from "../assets/images/apex.png"
+import Image from "next/image"
 
 const images = [
-  { src: acmeLogo, alt: "Acme Logo" },
-  { src: quantumLogo, alt: "Quantum Logo" },
-  { src: echoLogo, alt: "Echo Logo" },
-  { src: celestialLogo, alt: "Celestial Logo" },
-  { src: pulseLogo, alt: "Pulse Logo" },
-  { src: apexLogo, alt: "Apex Logo" },
-];
+  { src: acmeLogo, alt: "Acme Logo | SaaS Landing Page built by Diego Tech" },
+  {
+    src: quantumLogo,
+    alt: "Quantum Logo | SaaS Landing Page built by Diego Tech",
+  },
+  { src: echoLogo, alt: "Echo Logo | SaaS Landing Page built by Diego Tech" },
+  {
+    src: celestialLogo,
+    alt: "Celestial Logo | SaaS Landing Page built by Diego Tech",
+  },
+  { src: pulseLogo, alt: "Pulse Logo | SaaS Landing Page built by Diego Tech" },
+  { src: apexLogo, alt: "Apex Logo | SaaS Landing Page built by Diego Tech" },
+]
 
 export const LogoTicker = () => {
-  return null;
-};
+  return (
+    <section className="bg-black text-white">
+      <div className="container">
+        <h2>Trusted by the world&apos;s most innovative teams</h2>
+        <div>
+          <div className="flex">
+            {images.map(({ src, alt }) => (
+              <Image
+                key={alt}
+                src={src}
+                className="h-8 w-auto flex-none"
+                alt={alt}
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
