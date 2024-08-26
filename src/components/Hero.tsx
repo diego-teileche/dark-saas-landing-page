2e4fb21621eee558585556027e8f6243789da2b1
diego-teileche <160079120+diego-teileche@users.noreply.github.com>
2024-08-26 16:49:38 -0300

    Hero
    - Ancor added
    - Title added
    - Paragraph added
    - Button added

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,3 +1,20 @@
 export const Hero = () => {
-  return null;
-};
+  return (
+    <main>
+      <section className="bg-black text-white">
+        <div className="container">
+          <a href="#">
+            <span>Version 2.0 is here</span>
+            <span>Read More</span>
+          </a>
+          <h1>One Task at a Time</h1>
+          <p>
+            Celebrate the joy of accomplishment with an app designed to track
+            progress, motivate your efforts, and celebrate your successes
+          </p>
+          <button>Get for free</button>
+        </div>
+      </section>
+    </main>
+  )
+}
