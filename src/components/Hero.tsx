01e12b673f0ce22b787925728124914b8c9e5bb9
diego-teileche <160079120+diego-teileche@users.noreply.github.com>
2024-08-26 17:33:17 -0300

    Hero | Ancor tag styles and row icon added

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,12 +1,24 @@
+import ArrowWIcon from "@/assets/icons/arrow-w.svg"
+
 export const Hero = () => {
   return (
     <main>
-      <section className="bg-black text-white">
+      <section className="bg-[linear-gradient(to_bottom,#000,#200d42_34%,#4f21a1_65%,#a46edb_82%)] py-[72px] text-white">
         <div className="container">
-          <a href="#">
-            <span>Version 2.0 is here</span>
-            <span>Read More</span>
-          </a>
+          <div className="flex items-center justify-center">
+            <a
+              href="#"
+              className="inline-flex gap-3 rounded-lg border border-white/30 px-2 py-1"
+            >
+              <span className="bg-[linear-gradient(to_right,#f87aff,#fb93d0,#ffdd99,#c3f0b2,#2fd8fe)] bg-clip-text text-transparent">
+                Version 2.0 is here
+              </span>
+              <span className="inline-flex items-center gap-1">
+                <span>Read More</span>
+                <ArrowWIcon />
+              </span>
+            </a>
+          </div>
           <h1>One Task at a Time</h1>
           <p>
             Celebrate the joy of accomplishment with an app designed to track
