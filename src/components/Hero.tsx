ce5cbdec077d29e7c71c8a91c152b3476fd04194
diego-teileche <160079120+diego-teileche@users.noreply.github.com>
2024-08-26 18:20:31 -0300

    Hero | Responsive and Images
    - Cursor and Message images added
    - Responsive design styles added
    - More styles added

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,10 +1,13 @@
 import ArrowWIcon from "@/assets/icons/arrow-w.svg"
+import cursorImage from "@/assets/images/cursor.png"
+import messageImage from "@/assets/images/message.png"
+import Image from "next/image"
 
 export const Hero = () => {
   return (
     <main>
-      <section className="relative overflow-clip bg-[linear-gradient(to_bottom,#000,#200d42_34%,#4f21a1_65%,#a46edb_82%)] py-[72px] text-white">
-        <div className="absolute left-1/2 top-[calc(100%-96px)] h-[375px] w-[750px] -translate-x-1/2 rounded-[100%] border border-[#b48cde] bg-[radial-gradient(closest-side,#000_82%,#9560eb)]"></div>
+      <section className="relative overflow-clip bg-[linear-gradient(to_bottom,#000,#200d42_34%,#4f21a1_65%,#a46edb_82%)] py-[72px] text-white sm:py-24">
+        <div className="absolute left-1/2 top-[calc(100%-96px)] h-[375px] w-[750px] -translate-x-1/2 rounded-[100%] border border-[#b48cde] bg-[radial-gradient(closest-side,#000_82%,#9560eb)] sm:top-[calc(100%-120px)] sm:h-[768px] sm:w-[1536px] lg:h-[1200px] lg:w-[2400px]"></div>
         <div className="container relative">
           <div className="flex items-center justify-center">
             <a
@@ -20,13 +23,35 @@ export const Hero = () => {
               </span>
             </a>
           </div>
-          <h1 className="mt-8 text-center text-7xl font-bold tracking-tighter">
-            One Task at a Time
-          </h1>
-          <p className="mt-8 text-center text-xl">
-            Celebrate the joy of accomplishment with an app designed to track
-            your progress, motivate your efforts, and celebrate your successes
-          </p>
+          <div className="mt-8 flex justify-center">
+            <div className="relative inline-flex">
+              <h1 className="mt-8 inline-flex text-center text-7xl font-bold tracking-tighter sm:text-9xl">
+                One Task
+                <br />
+                at a Time
+              </h1>
+              <Image
+                src={cursorImage}
+                height="200"
+                width="200"
+                className="absolute right-[476px] top-[108px] hidden sm:inline"
+                alt="Cursor Image | SaaS Landing Page built by Diego Tech"
+              />
+              <Image
+                src={messageImage}
+                height="200"
+                width="200"
+                className="absolute left-[498px] top-[56px] hidden sm:inline"
+                alt="Message Image | SaaS Landing Page built by Diego Tech"
+              />
+            </div>
+          </div>
+          <div className="flex justify-center">
+            <p className="mt-8 max-w-md text-center text-xl">
+              Celebrate the joy of accomplishment with an app designed to track
+              your progress, motivate your efforts, and celebrate your successes
+            </p>
+          </div>
           <div className="mt-8 flex justify-center">
             <button className="rounded-lg bg-white px-5 py-3 font-medium text-black">
               Get for free
