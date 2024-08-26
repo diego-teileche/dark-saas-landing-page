8ce9159f5d1c13e410d8e98ff43bb9bd25569255
diego-teileche <160079120+diego-teileche@users.noreply.github.com>
2024-08-26 19:34:08 -0300

    Features Section | Styles
    - Title and Paragraph added
    - Features styles added
    - Responsive design styles added

--- src/components/Features.tsx
+++ src/components/Features.tsx
@@ -20,22 +20,29 @@ const features = [
 
 export const Features = () => {
   return (
-    <section className="bg-black text-white">
+    <section className="bg-black py-[72px] text-white sm:py-24">
       <div className="container">
-        <h2>Everything you need</h2>
-        <p>
-          Enjoy customizable lists, team work tools, and smart tracking all in
-          one place. Set tasks, get reminders, and see your progress simply and
-          quickly
-        </p>
-        <div>
+        <h2 className="text-center text-5xl font-bold tracking-tighter sm:text-6xl">
+          Everything you need
+        </h2>
+        <div className="mx-auto max-w-xl">
+          <p className="mt-5 text-center text-xl text-white/70">
+            Enjoy customizable lists, team work tools, and smart tracking all in
+            one place. Set tasks, get reminders, and see your progress simply
+            and quickly
+          </p>
+        </div>
+        <div className="mt-16 flex flex-col gap-4 sm:flex-row">
           {features.map(({ title, description }) => (
-            <div key={title}>
-              <div>
+            <div
+              key={title}
+              className="rounded-xl border border-white/30 px-5 py-10 text-center sm:flex-1"
+            >
+              <div className="inline-flex size-14 items-center justify-center rounded-lg bg-white text-black">
                 <EcosystemIcon />
               </div>
-              <h3>{title}</h3>
-              <p>{description}</p>
+              <h3 className="mt-6 font-bold">{title}</h3>
+              <p className="mt-2 text-white/70">{description}</p>
             </div>
           ))}
         </div>
