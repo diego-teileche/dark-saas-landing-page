507614dfada07bc07d092f7ecc7e2c17c1dbb3d1
diego-teileche <160079120+diego-teileche@users.noreply.github.com>
2024-08-26 13:02:45 -0300

    Navbar | Logo and menu icon styles added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,15 +4,20 @@ import Image from "next/image"
 
 export const Navbar = () => {
   return (
-    <header>
+    <header className="bg-black">
       <div className="container">
-        <div className="py-4">
-          <Image
-            src={logoImage}
-            className="size-12"
-            alt="SaaS Landing Page Logo | Diego Tech"
-          />
-          <MenuIcon />
+        <div className="flex items-center justify-between py-4">
+          <div className="relative">
+            <div className="absolute bottom-0 top-2 w-full bg-[linear-gradient(to_right,#f87bff,#f892cf,#ffdd98,#c2f081,#2fd8fe)] blur-md"></div>
+            <Image
+              src={logoImage}
+              className="relative size-12"
+              alt="SaaS Landing Page Logo | Diego Tech"
+            />
+          </div>
+          <div className="inline-flex size-10 items-center justify-center rounded-lg border border-white border-opacity-30">
+            <MenuIcon className="text-white" />
+          </div>
         </div>
       </div>
     </header>
