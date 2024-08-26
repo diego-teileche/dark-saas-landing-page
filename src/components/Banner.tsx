f2889c2b08e412445318bef34738a79ace636f1c
diego-teileche <160079120+diego-teileche@users.noreply.github.com>
2024-08-26 12:41:40 -0300

    Banner | Styles added

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,7 +1,16 @@
 export const Banner = () => {
   return (
-    <div className="container">
-      <a href="#">Explore the demo</a>
+    <div className="bg-[linear-gradient(to_right,rgb(252,214,255,.7),rgb(41,216,255,.7),rgb(255,253,128,.7),rgb(248,154,191,.7),rgb(252,214,255,.7))] py-3 text-center">
+      <div className="container">
+        <p className="font-medium">
+          <span className="hidden sm:inline">
+            Introducing a completely redesigned interface -{" "}
+          </span>
+          <a href="#" className="underline underline-offset-4">
+            Explore the demo
+          </a>
+        </p>
+      </div>
     </div>
   )
 }
