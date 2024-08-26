d5633af8f670f583bd0877a140668f9c5f61b21d
diego-teileche <160079120+diego-teileche@users.noreply.github.com>
2024-08-26 12:53:50 -0300

    Navbar | Logo and menu icon added

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,20 @@
+import logoImage from "@/assets/images/logosaas.png"
+import MenuIcon from "@/assets/icons/menu.svg"
+import Image from "next/image"
+
 export const Navbar = () => {
-  return null;
-};
+  return (
+    <header>
+      <div className="container">
+        <div className="py-4">
+          <Image
+            src={logoImage}
+            className="size-12"
+            alt="SaaS Landing Page Logo | Diego Tech"
+          />
+          <MenuIcon />
+        </div>
+      </div>
+    </header>
+  )
+}
