4673e6346a7876d68a64504b4ee59d9ea13a2b2f
diego-teileche <160079120+diego-teileche@users.noreply.github.com>
2024-08-26 12:17:00 -0300

    Tailwindcss file updated

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,4 @@
-import type { Config } from "tailwindcss";
+import type { Config } from "tailwindcss"
 
 const config: Config = {
   content: [
@@ -9,11 +9,11 @@ const config: Config = {
   theme: {
     extend: {
       container: {
-        padding: '1rem', 
+        padding: "1rem",
         center: true,
-      }
+      },
     },
   },
   plugins: [],
-};
-export default config;
+}
+export default config
