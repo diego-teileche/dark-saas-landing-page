e11492eef2ce82317091e21ec540be4034d82a9c
diego-teileche <160079120+diego-teileche@users.noreply.github.com>
2024-08-27 18:55:02 -0300

    Footer
    - Copy rights paragraph added
    - Social media icons added
    - Some basic styles added

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,3 +1,30 @@
+import InstaIcon from "@/assets/icons/insta.svg"
+import XSocial from "@/assets/icons/x-social.svg"
+import TiktokIcon from "@/assets/icons/tiktok.svg"
+import YoutubeIcon from "@/assets/icons/youtube.svg"
+
 export const Footer = () => {
-  return null;
-};
+  return (
+    <footer className="bg-black text-white">
+      <div className="container">
+        <div>
+          <div>&copy; 2024 Your Company, Inc. All rights reserved</div>
+          <ul>
+            <li>
+              <InstaIcon />
+            </li>
+            <li>
+              <XSocial />
+            </li>
+            <li>
+              <TiktokIcon />
+            </li>
+            <li>
+              <YoutubeIcon />
+            </li>
+          </ul>
+        </div>
+      </div>
+    </footer>
+  )
+}
