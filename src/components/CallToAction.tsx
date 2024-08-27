384dd01d1acb8ca8f34644a650e5780ebfb21df2
diego-teileche <160079120+diego-teileche@users.noreply.github.com>
2024-08-27 15:01:35 -0300

    CallToAction
    - Title added
    - Paragraph added
    - Form with input and button added
    - Some basic styles added

--- src/components/CallToAction.tsx
+++ src/components/CallToAction.tsx
@@ -1,3 +1,18 @@
 export const CallToAction = () => {
-  return null;
-};
+  return (
+    <section className="bg-black text-white">
+      <div className="container">
+        <h2>Get Instant access</h2>
+        <p>
+          Celebrate the joy of accomplishment with an app designed to track your
+          progress and motivate your efforts
+        </p>
+
+        <form action="">
+          <input type="email" placeholder="your@email.com" />
+          <button>Get access</button>
+        </form>
+      </div>
+    </section>
+  )
+}
