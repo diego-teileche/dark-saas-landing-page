ac66e2c42cb382720d27f75f9cef9452c4931ee7
diego-teileche <160079120+diego-teileche@users.noreply.github.com>
2024-08-27 18:36:59 -0300

    Call to Action | Responsive design styles added

--- src/components/CallToAction.tsx
+++ src/components/CallToAction.tsx
@@ -1,8 +1,8 @@
 export const CallToAction = () => {
   return (
-    <section className="bg-black py-[72px] text-center text-white">
-      <div className="container">
-        <h2 className="text-5xl font-bold tracking-tighter">
+    <section className="bg-black py-[72px] text-center text-white sm:py-24">
+      <div className="container relative max-w-xl">
+        <h2 className="text-5xl font-bold tracking-tighter sm:text-6xl">
           Get Instant access
         </h2>
         <p className="mt-5 text-lg text-white/70">
@@ -10,13 +10,13 @@ export const CallToAction = () => {
           progress and motivate your efforts
         </p>
 
-        <form className="mt-10 flex flex-col gap-2.5">
+        <form className="mx-auto mt-10 flex max-w-sm flex-col gap-2.5 sm:flex-row">
           <input
             type="email"
             placeholder="your@email.com"
-            className="h-12 rounded-lg bg-white/20 px-5 font-medium placeholder:text-[#9ca3af]"
+            className="h-12 flex-1 rounded-lg bg-white/20 px-5 font-medium placeholder:text-[#9ca3af]"
           />
-          <button className="h-12 rounded-lg bg-white font-medium text-black">
+          <button className="h-12 rounded-lg bg-white px-5 font-medium text-black">
             Get access
           </button>
         </form>
